0fb9e2bb9292148b6f70d03b82322c9172d10114
Omar Assidi <oia05@mail.aub.edu>
2024-09-08 22:26:06 +0300

    cleared project

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,30 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var firstDiceImageView: UIImageView!
-    @IBOutlet weak var secondDiceImageView: UIImageView!
-    @IBOutlet weak var rollButoon: UIButton!
-    
-    private let images = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        manageEvents()
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
-    
-    private func manageEvents() {
-        rollButoon.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.rollDice)))
-    }
-    
-    @objc private func rollDice() {
-        firstDiceImageView.image = getRandomDice()
-        secondDiceImageView.image = getRandomDice()
-    }
-    
-    private func getRandomDice() -> UIImage {
-        return images.randomElement()!!
-    }
-
-
 }
 
